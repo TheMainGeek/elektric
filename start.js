dc468b8055b0f41ef484520f47a0ccab90621052
TheMainGeek <49459814+TheMainGeek@users.noreply.github.com>
2021-03-05 12:35:31 -0500

    Added raw main.js link

--- start.js
+++ start.js
@@ -1,1 +1,1 @@
-javascript:var e = document.createElement("script");e.src = ""
+javascript:var e = document.createElement("script");e.src = "https://github.com/TheMainGeek/elektric/raw/main/main.js"
